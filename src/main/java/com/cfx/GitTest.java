c427081d6b09d186a203f9173b63069c5ac28d72
Cfx <cfx.com>
2022-10-29 17:23:40 +0800

    Second commit

--- src/main/java/com/cfx/GitTest.java
+++ src/main/java/com/cfx/GitTest.java
@@ -3,5 +3,6 @@ package com.cfx;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,github");
     }
 }
